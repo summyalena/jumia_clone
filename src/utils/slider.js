edd34decef65b9f1ec7c39d829654ce36b870af7
Lena <summyalena@gmail.com>
2023-08-24 15:35:39 +0100

    Add slider data

---
+++ src/utils/slider.js
@@ -0,0 +1,84 @@
+import Image1 from '../../public/assets/images/herosectionbeattherain.jpg';
+import Image2 from '../../public/assets/images/herosectionbagofrice.jpg';
+import Image3 from '../../public/assets/images/herosectionbeattherain.jpg';
+import Image4 from '../../public/assets/images/herosectiondesktop.png';
+import Image5 from '../../public/assets/images/herosectionofficalstores.jpg';
+import Image6 from '../../public/assets/images/herosectionshopandearn.png';
+
+import Image7 from '../../public/assets/images/officialstoreslide.jpg';
+import Image8 from '../../public/assets/images/officialstoreslide2.jpg';
+import Image9 from '../../public/assets/images/officialstoreslide3.jpg';
+import Image10 from '../../public/assets/images/officialstoreslide4.png';
+import Image11 from '../../public/assets/images/officialstoreslide5.jpg';
+import Image12 from '../../public/assets/images/officialstoreslide6.png';
+
+
+const herosectionSlider = [
+  {
+    id: 1,
+    name: 'slider1',
+    image: Image1
+  },
+  {
+    id: 2,
+    name: 'slider2',
+    image: Image2
+  },
+  {
+    id: 3,
+    name: 'slider3',
+    image: Image3
+  },
+  {
+    id: 4,
+    name: 'slider4',
+    image: Image4
+  },
+  {
+    id: 5,
+    name: 'slider5',
+    image: Image5
+  },
+  {
+    id: 6,
+    name: 'slider6',
+    image: Image6
+  }
+]
+
+export const slidertwo = [
+  {
+    id: 1,
+    name: '',
+    image: Image7
+  },
+  {
+    id: 2,
+    name: '',
+    image: Image8
+  },
+  {
+    id: 3,
+    name: '',
+    image: Image9
+  },
+  {
+    id: 4,
+    name: '',
+    image: Image10
+  },
+  {
+    id: 5,
+    name: '',
+    image: Image11
+  },
+  {
+    id: 6,
+    name: '',
+    image: Image12
+  },
+]
+
+
+
+export default herosectionSlider;
