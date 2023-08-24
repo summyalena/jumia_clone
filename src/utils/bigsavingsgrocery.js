2e595001c568ad0f52b4539ad4c536dd7aa8b5da
Lena <summyalena@gmail.com>
2023-08-24 15:33:07 +0100

    Add appliances and categories data

---
+++ src/utils/bigsavingsgrocery.js
@@ -0,0 +1,48 @@
+import image1 from '../../public/assets/images/imperialblue.jpg';
+import image2 from '../../public/assets/images/goldenmorn.jpg';
+import image3 from '../../public/assets/images/dusse.jpg';
+import image4 from '../../public/assets/images/sunlight.jpg';
+import image5 from '../../public/assets/images/ayoola.jpg';
+import image6 from '../../public/assets/images/orijinbitters.jpg';
+
+
+const bigsavingsgrocery = [
+  {
+    id: 1,
+    name: 'Imperial Blue Whisky...',
+    image: image1,
+    price: '#2400'
+  },
+  {
+    id: 2,
+    name: 'Nestle Golden Morn ke..',
+    image: image2,
+    price: '#2400'
+  },
+  {
+    id: 3,
+    name: 'DUSSE VSOP Cognac 75cl...',
+    image: image3,
+    price: '#2400'
+  },
+  {
+    id: 4,
+    name: 'Sunlight 2 in 1 tropical S...',
+    image: image4,
+    price: '#2400'
+  },
+  {
+    id: 5,
+    name: 'Ayoola Foods Cassava Fu...',
+    image: image5,
+    price: '#2400'
+  },
+  {
+    id: 6,
+    name: 'Orijin Bitters 20cl x24',
+    image: image6,
+    price: '#2400'
+  },
+]
+
+export default bigsavingsgrocery;
