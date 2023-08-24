8b84bf48170c110a49735f2729967f821f727038
Lena <summyalena@gmail.com>
2023-08-24 15:36:17 +0100

    Add data for sub sections in homepage

---
+++ src/utils/unilever.js
@@ -0,0 +1,42 @@
+import image1 from '../../public/assets/images/knorr.jpg';
+import image2 from '../../public/assets/images/sunlightpink.jpg';
+import image3 from '../../public/assets/images/sunlightyellow.jpg';
+import image4 from '../../public/assets/images/closeup.jpg';
+import image5 from '../../public/assets/images/sunlight.jpg';
+import image6 from '../../public/assets/images/sunlightsmall.jpg';
+
+
+const unilever = [
+  {
+    id: 1,
+    name: "Knorr Chicken Seasoning... ",
+    image: image1,
+  },
+  {
+    id: 2,
+    name: "Sunlight 2 in 1 Tropical...",
+    image: image2,
+  },
+  {
+    id: 3,
+    name: "Sunlight 2 in 1 Sun...",
+    image: image3,
+  },
+  {
+    id: 4,
+    name: "Closeup Complete Fresh...",
+    image: image4,
+  },
+  {
+    id: 5,
+    name: "Sunlight detergent Pow...",
+    image: image5,
+  },
+  {
+    id: 6,
+    name: "Sunlight 2 in 1 Sunshine...",
+    image: image6,
+  },
+];
+
+export default unilever;
