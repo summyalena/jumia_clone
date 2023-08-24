796464073aeb006896e9a007187f158aa51d253c
Lena <summyalena@gmail.com>
2023-08-24 15:34:21 +0100

    Add data

---
+++ src/utils/limitedstockdeals.js
@@ -0,0 +1,100 @@
+import image1 from '../../public/assets/images/limitedgalaxy.jpg';
+import image2 from '../../public/assets/images/limitedstandingfan.jpg';
+import image3 from '../../public/assets/images/limitedshoe.jpg';
+import image4 from '../../public/assets/images/limitedlp.jpg';
+import image5 from '../../public/assets/images/limitedsodas.jpg';
+import image6 from '../../public/assets/images/limitedpb.jpg';
+
+import image7 from '../../public/assets/images/mifi.jpg';
+import image8 from '../../public/assets/images/waterproofbt.jpg';
+import image9 from '../../public/assets/images/technospark.jpg';
+import image10 from '../../public/assets/images/adidasaqua.jpg';
+import image11 from '../../public/assets/images/oraimo.jpg';
+import image12 from '../../public/assets/images/manknitted.jpg';
+
+
+const limitedstockdeals = [
+  {
+    id: 1,
+    text: 'Samsung Galaxy A14 6.6"...',
+    image: image1,
+    price: '#650000',
+    slashedprice: '#750000'
+  },
+  {
+    id: 2,
+    text: 'Nexus 16 inches Standing...',
+    image: image2,
+    price: '#650000',
+    slashedprice: '#750000'
+  },
+  {
+    id: 3,
+    text: 'ADIDAS Vs Pace Lifestyle...',
+    image: image3,
+    price: '#650000',
+    slashedprice: '#750000'
+  },
+  {
+    id: 4,
+    text: 'Lenovo Ideapad 3 Intel C...',
+    image: image4,
+    price: '#650000',
+    slashedprice: '#750000'
+  },
+  {
+    id: 5,
+    text: 'Fanta Drink -50cl Pet x 12',
+    image: image5,
+    price: '#650000',
+    slashedprice: '#750000'
+  },
+  {
+    id: 6,
+    text: 'Power Banks 20000 MAh...',
+    image: image6,
+    price: '#650000',
+    slashedprice: '#750000'
+  }
+]
+
+export const officialstoredeals = [
+  {
+    id: 1,
+    text: '50% off & above',
+    image: image7,
+    price: '#60000'
+  },
+  {
+    id: 2,
+    text: 'Your Utility Store',
+    image: image8,
+    price: '#60000'
+  },
+  {
+    id: 3,
+    text: 'Your Jumia Food',
+    image: image9,
+    price: '#60000'
+  },
+  {
+    id: 4,
+    text: 'Up to 50% off',
+    image: image10,
+    price: '#60000'
+  },
+  {
+    id: 5,
+    text: 'Best Deals on drinks',
+    image: image11,
+    price: '#60000'
+  },
+  {
+    id: 6,
+    text: 'Official stores',
+    image: image12,
+    price: '#60000'
+  },
+]
+
+export default limitedstockdeals
