b6bf8197b905efa84d39239aca9a717331a647db
Lena <summyalena@gmail.com>
2023-08-24 15:35:47 +0100

    Add data

---
+++ src/utils/subcategories.js
@@ -0,0 +1,46 @@
+
+import image7 from '../../public/assets/images/bestprice.png';
+import image8 from '../../public/assets/images/jumiapay.png';
+import image9 from '../../public/assets/images/jumiafood.png';
+import image10 from '../../public/assets/images/phoneadapter.jpg';
+import image11 from '../../public/assets/images/exclusivedrinks.png';
+import image12 from '../../public/assets/images/officialstores.gif';
+
+
+const subcategories = [
+  {
+    id: 1,
+    text: '50% off & above',
+    text1: 'Best Price',
+    image: image7
+  },
+  {
+    id: 2,
+    text: 'Your Utility Store',
+    image: image8
+  },
+  {
+    id: 3,
+    text: 'Your Jumia Food',
+    image: image9
+  },
+  {
+    id: 4,
+    text: 'Up to 50% off',
+    text1: 'Phone Adapter',
+    image: image10
+  },
+  {
+    id: 5,
+    text: 'Best Deals on drinks',
+    image: image11
+  },
+  {
+    id: 6,
+    text: 'Official stores',
+    text1: 'Official Stores',
+    image: image12
+  }
+]
+
+export default subcategories;
