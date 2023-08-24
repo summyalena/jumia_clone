78579956a3eba40d27e33f3518e06c57a72e9d87
Lena <summyalena@gmail.com>
2023-08-24 15:33:48 +0100

    Add data for flashsales and officialstores

---
+++ src/utils/flashsales.js
@@ -0,0 +1,60 @@
+import image1 from '../../public/assets/images/flashBT-removebg-preview.png'
+import image2 from '../../public/assets/images/flashPhone-removebg-preview.png'
+import image3 from '../../public/assets/images/flashPB-removebg-preview.png'
+import image4 from '../../public/assets/images/flashPowerbanl-removebg-preview.png'
+import image5 from '../../public/assets/images/flashPERFS-removebg-preview.png'
+import image6 from '../../public/assets/images/flashTV.jpg'
+
+
+const flashsales = [
+  {
+    id: 1,
+    name: "Lenovo Bluetooth 5.3 Ear...",
+    images: image1,
+    price: "#6500",
+    slashedPrice: "#10000",
+    quantityleft: "30units",
+  },
+  {
+    id: 2,
+    name: "TECLAST P80 Tablet PC...",
+    images: image2,
+    price: "#6500",
+    slashedPrice: "#10000",
+    quantityleft: "30units",
+  },
+  {
+    id: 3,
+    name: "Oraimo 27000Mah Port..",
+    images: image3,
+    price: "#6500",
+    slashedPrice: "#10000",
+    quantityleft: "30units",
+  },
+  {
+    id: 4,
+    name: "itel 30000mah Triple Input",
+    images: image4,
+    price: "#6500",
+    slashedPrice: "#10000",
+    quantityleft: "30units",
+  },
+  {
+    id: 5,
+    name: "Givanas 136 Unisex Perf...",
+    images: image5,
+    price: "#6500",
+    slashedPrice: "#10000",
+    quantityleft: "30units",
+  },
+  {
+    id: 6,
+    name: "Infinity 32 Smart Frame...",
+    images: image6,
+    price: "#6500",
+    slashedPrice: "#10000",
+    quantityleft: "30units",
+  },
+];
+
+export default flashsales;
---
+++ src/utils/mainofficialstores.js
@@ -0,0 +1,116 @@
+import image1 from '../../public/assets/images/oraimostore.png';
+import image2 from '../../public/assets/images/Defacto.jpg';
+import image3 from '../../public/assets/images/tecnopng.png'
+import image4 from '../../public/assets/images/xiaomi.png'
+import image5 from '../../public/assets/images/adidas.png'
+import image6 from '../../public/assets/images/Nestle.png'
+import image7 from '../../public/assets/images/coca-cola.png'
+import image8 from '../../public/assets/images/diageo.png'
+import image9 from '../../public/assets/images/Pernod_ricard.png'
+import image10 from '../../public/assets/images/nivea.png'
+import image11 from '../../public/assets/images/infinix.png'
+import image12 from '../../public/assets/images/haier-thermocool.png'
+import image13 from '../../public/assets/images/philips.png'
+import image14 from '../../public/assets/images/samsung.png'
+import image15 from '../../public/assets/images/tcl.png'
+import image16 from '../../public/assets/images/weyon.png'
+import image17 from '../../public/assets/images/bacardi.png'
+import image18 from '../../public/assets/images/edifier.png'
+
+
+
+
+const mainofficialstores = [
+  {
+    id: 1,
+    name: "",
+    image: image1,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image2,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image3,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image4,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image5,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image6,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image7,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image8,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image9,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image10,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image11,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image12,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image13,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image14,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image15,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image16,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image17,
+  },
+  {
+    id: 1,
+    name: "",
+    image: image18,
+  },
+];
+
+export default mainofficialstores;
