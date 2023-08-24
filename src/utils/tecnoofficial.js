bded5126885929dc581b9e59d876ffd361aea8ee
Lena <summyalena@gmail.com>
2023-08-24 15:36:04 +0100

    Add data for tecno section in homepage

---
+++ src/utils/tecnoofficial.js
@@ -0,0 +1,62 @@
+import image1 from '../../public/assets/images/tecnopop7.jpg';
+import image2 from '../../public/assets/images/technospark.jpg';
+import image3 from '../../public/assets/images/tecnocamon.jpg';
+import image4 from '../../public/assets/images/tecnospark.jpg';
+import image5 from '../../public/assets/images/tecnospark10.jpg';
+import image6 from '../../public/assets/images/tecnopop.jpg';
+import image7 from '../../public/assets/images/tecnocamon20.jpg';
+
+
+const tecnoofficial = [
+  {
+    id: 1,
+    text: 'Tecno Pop 7 Pro 6.6" 4GB...',
+    image: image1,
+    price: '#158,000',
+    slashedprice: '#290,000'
+  },
+  {
+    id: 2,
+    text: 'Tecno Spark 10c 6.6" 4GB...',
+    image: image2,
+    price: '#158,000',
+    slashedprice: '#290,000'
+  },
+  {
+    id: 3,
+    text: 'Tecno Camon 20 6.67" 8G...',
+    image: image3,
+    price: '#158,000',
+    slashedprice: '#290,000'
+  },
+  {
+    id: 4,
+    text: 'Tecno Spark10 Pro 6.8" 8...',
+    image: image4,
+    price: '#158,000',
+    slashedprice: '#290,000'
+  },
+  {
+    id: 5,
+    text: 'Tecno POP 7(BF6) 6.6" H.',
+    image: image5,
+    price: '#158,000',
+    slashedprice: '#290,000'
+  },
+  {
+    id: 6,
+    text: 'Tecno Camon 20 6.67" 8G...',
+    image: image6,
+    price: '#158,000',
+    slashedprice: '#290,000'
+  },
+  {
+    id: 7,
+    text: 'Tecno Spark 10 Pro 6.8" 8',
+    image: image7,
+    price: '#158,000',
+    slashedprice: '#290,000'
+  },
+]
+
+export default tecnoofficial;
