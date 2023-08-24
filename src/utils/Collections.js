2c5061c3e3aab94cc777f138ff9f6f5601477e83
Lena <summyalena@gmail.com>
2023-08-24 15:33:26 +0100

    Add data for collections and health section

---
+++ src/utils/Collections.js
@@ -0,0 +1,75 @@
+import image1 from '../../public/assets/images/mobileaccessories.png';
+import image2 from '../../public/assets/images/computingdealscollection.jpg'
+import image3 from '../../public/assets/images/powerdeals.png'
+import image4 from '../../public/assets/images/kidscorner.png'
+import image5 from '../../public/assets/images/fitnessdeals.png'
+import image6 from '../../public/assets/images/gamingdeals.png';
+
+import image7 from '../../public/assets/images/phonedeals.png';
+import image8 from '../../public/assets/images/fashiondeals.png';
+import image9 from '../../public/assets/images/supermarket.jpg';
+import image10 from '../../public/assets/images/electronicsdeals.png';
+import image11 from '../../public/assets/images/appliancesdeals.png';
+import image12 from '../../public/assets/images/beautydeals.png';
+
+const Collections = [
+  {
+    id: 1,
+    image: image7
+  },
+  {
+    id: 2,
+    image: image8
+  },
+  {
+    id: 3,
+    image: image9
+  },
+  {
+    id: 4,
+    image: image10
+  },
+  {
+    id: 5,
+    image: image11
+  },
+  {
+    id: 6,
+    image: image12
+  }
+]
+
+export const morecollections = [
+  {
+    id:1,
+    name: 'Accessories',
+    image: image1
+  },
+  {
+    id:2,
+    name: 'Accessories',
+    image: image2
+  },
+  {
+    id:3,
+    name: 'Accessories',
+    image: image3
+  },
+  {
+    id:4,
+    name: 'Accessories',
+    image: image4
+  },
+  {
+    id:5,
+    name: 'Accessories',
+    image: image5
+  },
+  {
+    id:6,
+    name: 'Accessories',
+    image: image6
+  },
+]
+
+export default Collections;
